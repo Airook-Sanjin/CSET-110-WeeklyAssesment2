75bf364c20dec208a0ab08d24e69774d6ff068e0
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2024-10-16 13:16:48 -0400

    Fixed-addCart-cartbtnClickedfunction

--- Cart.js
+++ Cart.js
@@ -1,44 +1,37 @@
-
 let cartSection = document.getElementById("cart-section");
-
 let addtoCartBtn = document.getElementsByClassName("shop-item-button");
 let removeBtn = document.getElementsByClassName("btn-danger");
 let purchaseBtn = document.getElementsByClassName("btn-Purchase");
-let price = document.getElementsByClassName("shop-item-price");
-let total = document.getElementsByClassName("");
 let cartStyle = document.getElementsByClassName("cart-column");
-let cartItem = document.getElementsByClassName("cart-item");
-let cartImage = document.getElementsByClassName("cart-item-image");
-let cartTitle = document.getElementsByClassName("cart-item-title");
-let cartPrice = document.getElementsByClassName("cart-price");
+let quantityInputs = document.getElementsByClassName("cart-quantity-input");
 var clicked = false;
-
-function addtocartBtnClicked() {
-  if (!clicked) {
-    cartSection.style.display = "block";
-    clicked = true;
-  }
-}
-function addCart(title, price) {
-  let cartrow = document.createElement('div')
-  
+for (let i= 0; i <quantityInputs.length; i++){
+  let input = quantityInputs[i]
+  input.addEventListener('change ', quanitiyChanged) //make function
 }
 
-for (i = 0; i < addtoCartBtn.length; i++) {
-  addtoCartBtn[i].addEventListener("click", function () {
-    addtocartBtnClicked();
-
-    // addCart(item)
+for (let i = 0; i < addtoCartBtn.length; i++) {
+  var button = addtoCartBtn[i];
+  button.addEventListener("click", (event) => {
+    addtocartBtnClicked;
   });
 }
-for(let i = 0;i<addtoCartBtn.length; i++) {
-    addtoCartBtn[i].addEventListener('click' , (event) => {
-        alert(`you pressed"${i}"`)
-        let button = evt.target
-        let shopitem = button.parentElement.parentElement
-        let title = shopitem.getElementsByClassName('shop-item-title')[0].innerText
-        let price = shopitem.getElementsByClassName('shop-item-price')[0].innerText
-        
-    })
+function addtocartBtnClicked(event) {
+  var button = event.target;
+  let shopItem = button.parentElement.parentElement;
+  var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+  var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+  addCart(title, price)
 }
-
+function addCart(title, price) {
+  let cartrow = document.createElement("div");
+  cartrow.classList.add("cart-row");
+  let cartItems = document.getElementsByClassName("cart-items")[0];
+  let cartItemName = cartItems.getElementsByClassName("cart-item-title");
+  for (let i = 0; i < cartItemName.length; i++) {
+    if (cartItemName[i].innerText === title) {
+      alert("this item is already added to the cart");
+      return;
+    }
+  }
+}
